f7782808a9b0dd142d740fc920f81edefdf181cb
Frederico02 <81171707+Frederico02@users.noreply.github.com>
2022-12-14 00:06:42 -0300

    Add files via upload

---
+++ SAP_MULTI.cpp
@@ -0,0 +1,23 @@
+//*******************Atalho SAP *******************
+//Desenvolvido por Frederico de Jesus Almeida
+//Analista de Suporte Junior Multi
+//*******************12/12/2022******************
+
+#include <windows.h>
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+	
+	ShowWindow(GetConsoleWindow(), SW_HIDE); //prompt de comando
+	//system("start chrome.exe -silent -setDefaultBrowser"); TORNAR O GOOGLE PADRAO 
+	system("start https://s4qas.sap-multilaser.com.br/");
+
+	return 0;
+}
+
+
